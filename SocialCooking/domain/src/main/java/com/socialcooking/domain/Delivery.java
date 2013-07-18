a3b123c6d74a87cc272e289d7187073f05ac5ca5
Dzmitry Kislo <kislodim@gmail.com>
2013-07-18 13:14:44 +0300

    revert changelist

--- SocialCooking/domain/src/main/java/com/socialcooking/domain/Delivery.java
+++ SocialCooking/domain/src/main/java/com/socialcooking/domain/Delivery.java
@@ -4,15 +4,11 @@ package com.socialcooking.domain;
 public class Delivery {
 
     private Long id;
-
-
     private Integer price;
-
-
     private boolean isWhole;
-
-
     private Integer priceDelivery;
+
+
 
 
 
