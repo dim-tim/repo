372658ff57d70a9d1e1f64c380a5e266f42dbbfa
Mikalai Kisel <kisel.nikolay@gmail.com>
2013-07-31 15:58:20 +0300

    refactor

--- service/src/test/java/com/socialcooking/service/CommentServiceMockTest.java
+++ service/src/test/java/com/socialcooking/service/CommentServiceMockTest.java
@@ -5,22 +5,15 @@ import com.socialcooking.domain.Comment;
 import com.socialcooking.repository.CommentRepositoryImpl;
 import com.socialcooking.repository.api.CommentRepository;
 import com.socialcooking.repository.api.GenericRepository;
-import com.socialcooking.service.api.CommentService;
 import com.socialcooking.service.api.GenericService;
 import org.junit.Before;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import static org.junit.Assert.assertNotNull;
 
 public class CommentServiceMockTest extends AbstractGenericServiceMockTest<Comment>{
 
     @InjectMocks
     private CommentServiceImpl commentService = new CommentServiceImpl();
-
-    @Autowired
-    private CommentService commentService2;
 
     @Mock
     private CommentRepository commentRepository = new CommentRepositoryImpl();
@@ -46,10 +39,6 @@ public class CommentServiceMockTest extends AbstractGenericServiceMockTest<Comme
     public void init() {
         comment = new Comment();
         comment.setId(123L);
-    }
-
-    public void superTest() {
-        assertNotNull(commentService2);
     }
 
 
