eeb5238a359ab35f336944567a55feb5b13c90c3
Mikalai Kisel <kisel.nikolay@gmail.com>
2013-07-31 18:47:18 +0300

    add controller and web

--- controller/src/main/java/com/socialcooking/controller/UserController.java
+++ controller/src/main/java/com/socialcooking/controller/UserController.java
@@ -1,4 +1,11 @@
 package com.socialcooking.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/socialcooking/user")
+@Controller
 public class UserController {
+
+
 }
